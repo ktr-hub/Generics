6441a5e9393588798234941eedd6b54295d1d05c
KUMMARI TIRUPATHI RAO <35338095+ktr-hub@users.noreply.github.com>
2020-10-20 09:59:11 +0530

    [Tirupathi] Add. Welcome message

---
+++ GenericsProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GenericsProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Generics Problem!");
+        }
+    }
+}
