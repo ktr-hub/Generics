0dba3235820002245fee0d78b4c629fb4984a944
KUMMARI TIRUPATHI RAO <35338095+ktr-hub@users.noreply.github.com>
2020-10-20 10:19:04 +0530

    [Tirupathi] Add. Test case for maximum among 3 double numbers

--- GenericsProject/GenericsMain.cs
+++ GenericsProject/GenericsMain.cs
@@ -4,15 +4,31 @@ namespace GenericsProject
 {
     public class GenericsMain
     {
-        public int getMax(int a,int b,int c)
+        public int getIntMax(int a,int b,int c)
         {
+            if(a==b && b == c)
+            {
+                throw new Exception("All are same");
+            }
             int max = (a > b) ? a : b;
             max = (max > c) ? max : c;
             return max;
         }
+
+        public double getDoubleMax(double a, double b, double c)
+        {
+            if (a == b && b == c)
+            {
+                throw new Exception("All are same");
+            }
+            double max = (a > b) ? a : b;
+            max = (max > c) ? max : c;
+            return max;
+        }
+
         static void Main(string[] args)
         {
-
+            Console.WriteLine("Welcome to generics test case problem...");
         }
     }
 }
--- UnitTestProjectGenerics/UnitTest1.cs
+++ UnitTestProjectGenerics/UnitTest1.cs
@@ -7,13 +7,24 @@ namespace UnitTestProjectGenerics
     [TestClass]
     public class UnitTest1
     {
+        //UC-1
         [TestMethod]
-        public void TestMethod1()
+        public void TestMethodForIntMax()
         {
-            int expected = 3;
+            int expected = 9;
             GenericsMain program = new GenericsMain();
 
-            int result = program.getMax(3, 2, 1);
+            int result = program.getIntMax(3, 9, 3);
+            Assert.AreEqual(expected, result);
+        }
+        //UC-2
+        [TestMethod]
+        public void TestMethodForDoubleMax()
+        {
+            double expected = 9.3;
+            GenericsMain program = new GenericsMain();
+
+            double result = program.getDoubleMax(3.2, 9.3, 3.4);
             Assert.AreEqual(expected, result);
         }
     }
